bc8724ea4422ec25da237733dcd6b1549115f6ce
Kostik Lapanik <lapanik.kostik@gmail.com>
2025-04-05 11:56:52 +0300

    added rendering of the registration form and checking of its fields

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { firebase } from "./firebaseActions.js";
 export { initMap } from './map/initMap.js';
-export { createTransportContainer } from './transportContainer/transportContainer.js'
+export { createTransportContainer } from './transportContainer/transportContainer.js';
+export { registerUser } from './forms/forms.js'
