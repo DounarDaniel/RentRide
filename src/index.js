a32bed04a14ad5ff32785d35d57a520378942708
Kostik Lapanik <lapanik.kostik@gmail.com>
2025-04-05 13:26:15 +0300

    add password encryption

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 export { firebase } from "./firebaseActions.js";
 export { initMap } from './map/initMap.js';
 export { createTransportContainer } from './transportContainer/transportContainer.js';
-export { registerUser } from './forms/forms.js'
+export { registerUser } from './forms/forms.js';
+export { encryptPassword } from './encryption.js';
+export { decryptPassword } from './encryption.js'
