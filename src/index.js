bae2674813c1fbb1c59838871bb30c17a0b16f25
Kostik Lapanik <lapanik.kostik@gmail.com>
2025-04-05 13:26:15 +0300

    add password encryption

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 export { firebase } from "./firebaseActions.js";
 export { initMap } from './map/initMap.js';
 export { createTransportContainer } from './transportContainer/transportContainer.js';
+<<<<<<< HEAD
 export { registerUser } from './forms/register/registerUser.js';
 export { logInUser } from './forms/logIn/logInUser.js';
+=======
+export { registerUser } from './forms/forms.js';
+>>>>>>> b33ef1b (add password encryption)
 export { encryptPassword } from './encryption.js';
 export { decryptPassword } from './encryption.js'
