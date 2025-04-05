52b5db265f139f50778eddbc79db4e08fe362120
Kostik Lapanik <lapanik.kostik@gmail.com>
2025-04-05 16:27:39 +0300

    fit: add lodIn form logic

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ export { firebase } from "./firebaseActions.js";
 export { initMap } from './map/initMap.js';
 export { createTransportContainer } from './transportContainer/transportContainer.js';
 <<<<<<< HEAD
+<<<<<<< HEAD
 export { registerUser } from './forms/register/registerUser.js';
 export { logInUser } from './forms/logIn/logInUser.js';
+=======
+export { registerUser } from './forms/forms.js';
+export { logInUser } from './forms/forms.js'
+>>>>>>> 5b577db (fit: add lodIn form logic)
 export { encryptPassword } from './encryption.js';
 export { decryptPassword } from './encryption.js'
 =======
