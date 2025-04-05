1fad44378d4bfe72b63eaed6b83709dc6ac03334
Kostik Lapanik <lapanik.kostik@gmail.com>
2025-04-05 11:56:52 +0300

    added rendering of the registration form and checking of its fields

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-import { initMap, createTransportContainer } from "./src/index.js";
-initMap();
-createTransportContainer();
+import { initMap, createTransportContainer, registerUser } from "./src/index.js";
+
+const userData = JSON.parse(localStorage.getItem('userData'));
+
+if (!userData || !userData.isLoged) {
+    registerUser()  
+} else {
+    initMap();
+    createTransportContainer();
+}
