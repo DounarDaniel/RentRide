72331c9a0fd01360c2ef2fe99334ba37fbc2e109
Kostik Lapanik <lapanik.kostik@gmail.com>
2025-04-05 11:56:52 +0300

    added rendering of the registration form and checking of its fields

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 export { firebase } from "./firebaseActions.js";
 export { initMap } from './map/initMap.js';
 export { createTransportContainer } from './transportContainer/transportContainer.js';
+<<<<<<< HEAD
 export { registerUser } from './forms/register/registerUser.js';
 export { logInUser } from './forms/logIn/logInUser.js';
 export { encryptPassword } from './encryption.js';
 export { decryptPassword } from './encryption.js'
+=======
+export { registerUser } from './forms/forms.js'
+>>>>>>> bc8724e (added rendering of the registration form and checking of its fields)
