3362a629d9cf7fdd7bb32eed80882bae1d74d1c0
Kostik Lapanik <lapanik.kostik@gmail.com>
2025-04-05 16:27:39 +0300

    fit: add lodIn form logic

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import { initMap, createTransportContainer, registerUser } from "./src/index.js";
+import { logInUser } from "./src/index.js";
 
-const userId = localStorage.getItem('userId');
+// const userId = localStorage.getItem('userId');
 
-if (!userId) {
-    registerUser()  
-} else {
-    initMap();
-    createTransportContainer();
-}
+// if (!userId) {
+//     registerUser()  
+// } else {
+//     initMap();
+//     createTransportContainer();
+// }
+
+console.log(logInUser())
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ export { firebase } from "./firebaseActions.js";
 export { initMap } from './map/initMap.js';
 export { createTransportContainer } from './transportContainer/transportContainer.js';
 export { registerUser } from './forms/forms.js';
+export { logInUser } from './forms/forms.js'
 export { encryptPassword } from './encryption.js';
 export { decryptPassword } from './encryption.js'
