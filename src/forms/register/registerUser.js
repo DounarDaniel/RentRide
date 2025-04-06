eeb24f2f0c78d8fc6e9319f3510b43eaa2e07feb
Kostik Lapanik <lapanik.kostik@gmail.com>
2025-04-06 20:14:50 +0300

    fixed mistakes

--- src/forms/register/registerUser.js
+++ src/forms/register/registerUser.js
@@ -1,9 +1,5 @@
 import { drawRegisterForm } from "./drawRegisterForm.js";
-<<<<<<< HEAD
 import { firebase, encryptPassword, initMap, createTransportContainer } from "../../index.js";
-=======
-import { firebase, encryptPassword } from "../../index.js";
->>>>>>> 73b50af (redesigned the project architecture)
 import { USERS_COLLECTION_NAME, USERS_DOC_ID, DEFAULT_AVATAR } from "../../constants.js";
 
 export function registerUser() {
@@ -16,12 +12,8 @@ export function registerUser() {
     form.addEventListener('submit', async function (event) {
         event.preventDefault()
 
-<<<<<<< HEAD
         const form = event.target
         const formsElements = form.elements;
-=======
-        const formsElements = event.target.elements;
->>>>>>> 73b50af (redesigned the project architecture)
 
         const passwordInput = formsElements.password;
         const confirmPasswordInput = formsElements.confirm_password;
@@ -76,13 +68,10 @@ export function registerUser() {
         }
 
         firebase.addDataToFirebase(USERS_COLLECTION_NAME, USERS_DOC_ID, 'users', firebaseUserData);
-<<<<<<< HEAD
 
         form.remove();
         initMap();
         createTransportContainer();
-=======
->>>>>>> 73b50af (redesigned the project architecture)
     })
 }
 
