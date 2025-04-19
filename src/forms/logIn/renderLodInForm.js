52c94eccd242babcdf933c24732629e1bb220566
Gibholk <novitnijmatvej@gmail.com>
2025-04-19 17:56:23 +0300

    start doing eye button

--- src/forms/logIn/renderLodInForm.js
+++ src/forms/logIn/renderLodInForm.js
@@ -15,13 +15,16 @@ export function renderLogInForm(container) {
         <div class=${styles.inputGroup}>
             <label for="password">Password</label>
             <input type="password" id="password" name="password" placeholder="••••••••" required>
+            <button type="button" class=${styles.togglePassword}>
+                <img src="./visibility_off.png" alt="closeEye">
+            </button>
         </div>
 
         <button type="submit">Sign In</button>
         <p class=${styles.loginText}>No account? <a href="#" class=${styles.loginLink} id="registerLink">Create an account</a></p>
     </form>`
 
-    if(document.forms.length > 0 && !!document.forms.register){
+    if (document.forms.length > 0 && !!document.forms.register) {
         document.forms.register.remove();
     }
 
@@ -31,4 +34,26 @@ export function renderLogInForm(container) {
         document.forms.logIn.remove();
         registerUser();
     })
+
+    // const passwordInput = document.getElementById("password");
+    // const toggleButton = document.getElementById("togglePassword");
+
+    // toggleButton.addEventListener("click", function () {
+    //     const openEye = `<img src="./visibility_off.png" alt="openEye">`;
+    //     const closeEye = `<img src="./visibility.png" alt="closeEye">`;
+
+    //     if (passwordInput.type === "password") {
+    //         if (toggleButton.children.length > 0) {
+    //             toggleButton.innerHTML = "";
+    //         }
+    //         passwordInput.type = "text";
+    //         toggleButton.insertAdjacentHTML('afterbegin', openEye);
+    //     } else {
+    //         if (toggleButton.children.length > 0) {
+    //             toggleButton.innerHTML = "";
+    //         }
+    //         passwordInput.type = "password";
+    //         toggleButton.insertAdjacentHTML('afterbegin', closeEye);
+    //     }
+    // });
 }
