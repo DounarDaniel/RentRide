3f6e0acd5b9290a39f3b22e1d812b8410c8ea4d7
Kostik Lapanik <lapanik.kostik@gmail.com>
2025-05-24 14:25:38 +0300

    feat: add grid for wrapper

--- src/transportCard/wrapper/renderCardsWrapper.js
+++ src/transportCard/wrapper/renderCardsWrapper.js
@@ -1,8 +1,8 @@
-import { wrapper } from '../transportCard.module.css'
+import styles from './wrapper.module.css'
 
 export function renderCardsWrapper() {
     const transportCardWrapper = document.createElement('section');
-    transportCardWrapper.classList.add(wrapper);
+    transportCardWrapper.classList.add(styles.wrapper);
     transportCardWrapper.setAttribute('id', 'transportCardWrapper');
 
     return transportCardWrapper;
